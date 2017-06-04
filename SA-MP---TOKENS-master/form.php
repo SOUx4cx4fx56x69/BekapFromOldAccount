1515404f6bbcd5fcc19596ce0fefe67009135e3b
Warlock-Dalbaeb <a.yvarov@yandex.ru>
2017-06-04 19:10:45 +0700

    Update form.php

--- SA-MP---TOKENS-master/form.php
+++ SA-MP---TOKENS-master/form.php
@@ -1,13 +1,11 @@
-
 <?php
 /*
 ...Warlock-Dalbaeb...
 Xyuta 17.?.17
 */
-require_once( "tokens.class.php" );
-$token = new tokens( "./fonts/arial.ttf" , "img/".$_GET['w'].".png" );
-$token->writeText($_GET['n']);
+require_once("tokens.class.php");
+$token = new tokens("./fonts/arial.ttf","img/".$_GET['w'].".png");
 header( 'Content-Type: image/png' );
+$token->writeNumbers($_GET['n']);
 $token->getImage();
-$token=NULL;
-?>
+
